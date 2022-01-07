2496e55a311b8de94cc40f90039c9074d0c2c5be
Deathstar-git <79025632112tch@gmail.com>
2022-01-07 19:04:56 +0700

    almost final version

--- content.js
+++ content.js
@@ -1,86 +1,227 @@
+let subjects = [];
+let tasks = [];
+let username;
 
-if( document.readyState !== 'loading' ) {
-    console.log( 'Документ уже загружен' );
-    setTimeout(function() {
-  myInitCode();
-}, 3000);
-} else {
-    document.addEventListener('DOMContentLoaded', function () {
-        console.log( 'Документ только грузится' );
-        setTimeout(function() {
-  myInitCode();
-}, 3000);
-    });
+function getSubjects() {
+    let link = $("a.aalink.coursename.mr-2");
+    var name_link = $(".page-header-headings");
+    username = name_link[0].innerHTML.slice(4, -5).split(" ")[1];
+    for (let i = link.length - 2; i >= 0; i--) {
+        subjects[i] = {
+            'name': link[i].innerText.substr(15),
+            'href': link[i]['href']
+        };
+    }
+    ;
 }
-function getElement(url, selector, c) {
-    request(new XMLHttpRequest());
 
-    function request(xhr) {
-        xhr.open('GET', url, true);
+function getTasks() {
+    let task_selector = '.gradeitemheader';
+    let tasks_elem;
+    let statuses_elem;
+
+    subjects.forEach(function (sub) {
+        let course_id = sub.href.replace(/[^0-9]/g, "");
+        let url = 'https://e.sfu-kras.ru/grade/report/user/index.php?id=' + course_id;
+
+        let xhr = new XMLHttpRequest();
+        xhr.open('GET', url);
         xhr.send();
-        xhr.onreadystatechange = function() {
-            if(xhr.readyState == 4) {
-                if(xhr.status == 200) {
-                    html = document.createElement('div');
-                    html.innerHTML = xhr.responseText;
-                    c(html.querySelectorAll(selector));
-                }
+
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState == 4 && xhr.status == 200) {
+                html = document.createElement('div');
+                html.innerHTML = xhr.responseText;
+                tasks_elem = html.querySelectorAll(task_selector);
+                statuses_elem = html.querySelectorAll('.level1.levelodd.oddd1.baggt.b2b.itemcenter.column-grade, .level2.leveleven.item.b1b.itemcenter.column-grade, .level2.leveleven.oddd2.baggt.b2b.itemcenter.column-grade, .level2.leveleven.evend2.baggt.b2b.itemcenter.column-grade, .level3.levelodd.item.b1b.itemcenter.column-grade'
+                );
+
+                tasks_elem.forEach(function (t, i) {
+                    tasks.push(
+                        {
+                            'name': t.title.replace("Ссылка на элемент курса ", ""),
+                            'href': t.href,
+                            'subject': sub.name,
+                            'status': statuses_elem[i].innerHTML
+                        })
+                });
             }
         }
-    }
-}
-function myInitCode() {
-	var link = $("a.aalink.coursename.mr-2");
-	var name_link = $(".page-header-headings")
-	var name = name_link[0].innerHTML.slice(4, -5).split(" ")[1];
-	var subjects = [];
-	
-    for (var i = link.length - 2; i >= 0; i--) {
-		let tasks = [];
-		let href = link[i].href;
-		let course_id = href.replace(/[^0-9]/g,"");
-		getElement('https://e.sfu-kras.ru/grade/report/user/index.php?id=' + course_id, '.gradeitemheader', function(element) {
-			for(var j = element.length - 1; j >=0; j--) {
-				let task_check = element[j].title.includes("Итого");
-				if (task_check == false) {
-					tasks[j] = element[j].title.replace("Ссылка на элемент курса ", "");
-				}
-				
-			}
-		});
-		subjects[i] = {
-			'name':link[i].innerText.substr(15),
-			 'tasks':tasks};
-     }; 
-	 var data = {
-		'name': name,
-		'subjects': subjects
 
-	};
-	console.log(data);
-	let p = data.subjects[0];
-	console.log(p);
-	setTimeout(function() {
-		chrome.storage.sync.set({ data });
-		chrome.runtime.sendMessage(data);
-	  }, 6000);
-	
-	
-	
-}
-
-let textNodes = [];
-function recursion(element) {
-	element.childNodes.forEach(node => {
-		if (node.nodeName.match(/^SPAN/)) {
-			console.log(node);
-			console.log(node.nodeValue);
-			console.log(node.q)
-			textNodes.push(node.textContent);
-		} else {
-			recursion(node);
-		}
-	});
+    });
 }
 
 
+if (document.readyState !== 'loading') {
+    console.log('Документ уже загружен');
+
+    setTimeout(function () {
+        getSubjects();
+    }, 3000);
+    setTimeout(function () {
+        getTasks();
+    }, 3000);
+    setTimeout(function () {
+        var data = {
+            'name': username,
+            'subjects': subjects,
+            'tasks': tasks,
+        }
+        chrome.runtime.sendMessage(data);
+        console.log(data);
+    }, 20000);
+} else {
+    document.addEventListener('DOMContentLoaded', function () {
+        console.log('Документ только грузится');
+        setTimeout(function () {
+            getSubjects();
+        }, 3000);
+
+    });
+}
+
+/////////////////////////////////////////////
+
+// if( document.readyState !== 'loading' ) {
+//     console.log( 'Документ уже загружен' );
+//     setTimeout(function() {
+//         myInitCode();
+//     }, 3000);
+// } else {
+//     document.addEventListener('DOMContentLoaded', function () {
+//         console.log( 'Документ только грузится' );
+//         setTimeout(function() {
+//             myInitCode();
+//         }, 3000);
+//     });
+// }
+
+// var tasks = [];
+// var subjects = [];
+// function myInitCode() {
+//     var link = $("a.aalink.coursename.mr-2");
+//     var name_link = $(".page-header-headings")
+//     var name = name_link[0].innerHTML.slice(4, -5).split(" ")[1];
+//     for (var i = link.length - 2; i >= 0; i--) {
+//
+//         let href = link[i].href;
+//         let course_id = href.replace(/[^0-9]/g,"");
+//
+//         getElement('https://e.sfu-kras.ru/grade/report/user/index.php?id=' + course_id, '.gradeitemheader', function(element) {
+//             for(var j = element.length - 1; j >=0; j--) {
+//                 let task_check = element[j].title.includes("Итого");
+//                 if (task_check == false) {
+//                     if (element[j].href == undefined) {
+//
+//                     } else {
+//                         tasks[j] = {
+//                             'name':element[j].title.replace("Ссылка на элемент курса ", ""),
+//                             'href':element[j].href,
+//                             'status': getElement(element[j].href, ".lastcol", function(element){
+//                             })
+//                         };
+//
+//
+//                     };
+//
+//                 };
+//
+//
+//             };
+//             console.log(element);
+//
+//         });
+//
+//     };
+//
+//
+//     setTimeout(function() {
+//         var link = $("a.aalink.coursename.mr-2");
+//         for(var i = link.length - 2; i >= 0; i--) {
+//             subjects[i] = {
+//                 'name':link[i].innerText.substr(15),
+//                 'tasks':tasks
+//             };
+//         };
+//         var data = {
+//             'name': name,
+//             'subjects': subjects
+//         };
+//         chrome.runtime.sendMessage(data);
+//     }, 15000);
+// };
+
+
+// function getElement(url, selector) {
+//     let elem;
+//
+//     let xhr = new XMLHttpRequest();
+//     xhr.open('GET', url);
+//     xhr.send();
+//
+//     xhr.onreadystatechange = function () {
+//         if (this.readyState == 4 && this.status == 200) {
+//             html = document.createElement('div');
+//             html.innerHTML = xhr.responseText;
+//             console.log(html);
+//             elem = html.querySelectorAll(selector);
+//             console.log(elem);
+//         }
+//     }
+//
+//     console.log(elem)
+//     return elem;
+// }
+// function getElement(url, selector) {
+//     let elem;
+//     $.get(url, function (data){
+//         elem = $(selector, data).html();
+//         console.log(elem);
+//     });
+// }
+// function getElement(url, selector, c, store, key) {
+//     request(new XMLHttpRequest());
+//     let elem;
+//
+//     function request(xhr) {
+//         xhr.open('GET', url, true);
+//         xhr.send();
+//         xhr.onreadystatechange = function () {
+//             if (xhr.readyState == 4 && xhr.status == 200) {
+//                 html = document.createElement('div');
+//                 html.innerHTML = xhr.responseText;
+//                 elem = html.querySelectorAll(selector);
+//                 c(store, key);
+//             }
+//         }
+//     }
+// }
+// function getStatuses() {
+//     let selector = '.lastcol';
+//     let elem;
+//
+//     tasks.forEach(function (task) {
+//         let url = task.href;
+//
+//         let xhr = new XMLHttpRequest();
+//         xhr.open('GET', url);
+//         xhr.send();
+//
+//         xhr.onreadystatechange = function () {
+//             if (xhr.readyState == 4 && xhr.status == 200) {
+//                 html = document.createElement('div');
+//                 html.innerHTML = xhr.responseText;
+//                 elem = html.querySelectorAll(selector);
+//                 console.log(elem);
+//
+//
+//             }
+//         }
+//     });
+// }
+// function getTasks() {
+//     subjects.forEach(function (sub) {
+//         let course_id = sub.href.replace(/[^0-9]/g, "");
+//         getElement('https://e.sfu-kras.ru/grade/report/user/index.php?id=' + course_id, '.gradeitemheader', key=sub.name);
+//     });
+// }
--- popup.js
+++ popup.js
@@ -1,47 +1,152 @@
-
 document.addEventListener('DOMContentLoaded', function () {
-	var default_data;
-	chrome.storage.sync.get("data", ({ data }) => {
-		default_data = data;
-	  });
-	document.querySelector(".info").innerHTML = "Привет," + default_data.name + "!";
-	var login_link = document.getElementById('login_link');
-	login_link.parentNode.removeChild(login_link);
-	for (var i = default_data.subjects.length - 2; i >= 0; i--) {
-		const $newLi = document.createElement('li');
-		$newLi.textContent = default_data.subjects[i].name;
-		const $colors = document.querySelector('#subjects');
-		const $tasks = document.querySelector('#tasks');
-		$colors.appendChild($newLi);
-		for (var j = default_data.subjects[i].tasks.length - 1; j >= 0; j--) {
-			const $newTask = document.createElement('li');
-			let subjects = data.subjects[i];
-			$newTask.textContent = subjects.tasks[j];
-			$tasks.appendChild($newTask);
-		}
-		
-	  }
+    var default_data = JSON.parse(localStorage.getItem('my_data'));
+    // chrome.storage.local.get(['my_data'], ({data}) => {
+    //     default_data = data;
+    // });
+    document.querySelector(".username-div").innerHTML = "Привет," + default_data.name + "!";
+    if (document.getElementById('login_link') !== null) {
+        var login_link = document.getElementById('login_link');
+        login_link.parentNode.removeChild(login_link);
+    }
+    for (var i = 0; i <= default_data.subjects.length - 1; i++) {
+        const $newSubject = document.createElement('div');
+        
+        $newSubject.classList.add('subject');
+        
+        const $newSubjectName = document.createElement('p');
+        $newSubjectName.textContent = default_data.subjects[i].name;
+        $newSubject.appendChild($newSubjectName);
+
+        const $subjects = document.querySelector('#subjects');
+        $subjects.appendChild($newSubject);
+        for (var j = 0; j <= default_data.tasks.length - 1; j++) {
+            if(default_data.tasks[j].subject === default_data.subjects[i].name) {
+                const $newTaskDiv = document.createElement('div');
+                const $newTask = document.createElement('a');
+                $newTask.href = default_data.tasks[j].href;
+                let task_status = default_data.tasks[j].status;
+                if (task_status == "Ответы для оценки" || task_status == "Оценено") {
+                    $newTaskDiv.classList.add('success');
+                }
+                if (task_status == "-") {
+                    $newTaskDiv.classList.add('error');
+                }
+                else {
+                    $newTaskDiv.classList.add('success');
+                }
+                $newTask.textContent = default_data.tasks[j].name;
+                if (default_data.tasks[j].name.includes('Задание')){
+                 
+                    $TaskImg = document.createElement('i');
+                    $TaskImg.classList.add('fas');
+                    $TaskImg.classList.add('fa-tasks');
+                    $newTaskDiv.appendChild($TaskImg);
+
+                };
+                if (default_data.tasks[j].name.includes('Тест')){
+                 
+                    $TaskImg = document.createElement('i');
+                    $TaskImg.classList.add('far');
+                    $TaskImg.classList.add('fa-edit');
+                    $newTaskDiv.appendChild($TaskImg);
+
+                };
+                if (default_data.tasks[j].name.includes('Лекция')){
+                   
+                    $TaskImg = document.createElement('i');
+                    $TaskImg.classList.add('fas');
+                    $TaskImg.classList.add('fa-book-open');
+                    $newTaskDiv.appendChild($TaskImg);
+                };
+                $newTaskDiv.appendChild($newTask);
+                $newSubject.appendChild($newTaskDiv);
+                $newSubject.appendChild(document.createElement('br'));
+            }
+        }
+
+    }
 });
+
 chrome.runtime.onMessage.addListener(
-	function(response, sender, sendResponse) {
+    function (response, sender, sendResponse) {
 
-	  let data  = response;
-	  document.querySelector(".info").innerHTML = "Привет," + data.name + "!";
-	  var login_link = document.getElementById('login_link');
-      login_link.parentNode.removeChild(login_link);
-	  for (var i = data.subjects.length - 2; i >= 0; i--) {
-		const $newLi = document.createElement('li');
-		$newLi.textContent = data.subjects[i].name;
-		const $colors = document.querySelector('#subjects');
-		const $tasks = document.querySelector('#tasks');
-		$colors.appendChild($newLi);
-		for (var j = data.subjects[i].tasks.length - 1; j >= 0; j--) {
-			const $newTask = document.createElement('li');
-			let subjects = data.subjects[i];
-			$newTask.textContent = subjects.tasks[j];
-			$tasks.appendChild($newTask);
-		}
-	  }
-	}
-  );
+        let data = response;
+        localStorage.setItem('my_data', JSON.stringify(data));
+        document.querySelector(".username-div").innerHTML = "Привет," + data.name + "!";
+        if (document.getElementById('login_link') !== null) {
+            var login_link = document.getElementById('login_link');
+            login_link.parentNode.removeChild(login_link);
+        };
+        for (var i = 0; i <= data.subjects.length - 1; i++) {
+        const $newSubject = document.createElement('div');
+        
+        $newSubject.classList.add('subject');
+        
+        const $newSubjectName = document.createElement('p');
+        $newSubjectName.textContent = data.subjects[i].name;
+        $newSubject.appendChild($newSubjectName);
 
+        const $subjects = document.querySelector('#subjects');
+        $subjects.appendChild($newSubject);
+        for (var j = 0; j <= data.tasks.length - 1; j++) {
+            if(data.tasks[j].subject === data.subjects[i].name) {
+                const $newTaskDiv = document.createElement('div');
+                const $newTask = document.createElement('a');
+                $newTask.href = data.tasks[j].href;
+                let task_status = data.tasks[j].status;
+                if (task_status == "Ответы для оценки" || task_status == "Оценено") {
+                    $newTaskDiv.classList.add('success');
+                }
+                if (task_status == "-") {
+                    $newTaskDiv.classList.add('error');
+                }
+                else {
+                    $newTaskDiv.classList.add('success');
+                }
+                $newTask.textContent = data.tasks[j].name;
+                if (data.tasks[j].name.includes('Задание')){
+                 
+                    $TaskImg = document.createElement('i');
+                    $TaskImg.classList.add('fas');
+                    $TaskImg.classList.add('fa-tasks');
+                    $newTaskDiv.appendChild($TaskImg);
+
+                };
+                if (data.tasks[j].name.includes('Тест')){
+                 
+                    $TaskImg = document.createElement('i');
+                    $TaskImg.classList.add('far');
+                    $TaskImg.classList.add('fa-edit');
+                    $newTaskDiv.appendChild($TaskImg);
+
+                };
+                if (data.tasks[j].name.includes('Лекция')){
+                   
+                    $TaskImg = document.createElement('i');
+                    $TaskImg.classList.add('fas');
+                    $TaskImg.classList.add('fa-book-open');
+                    $newTaskDiv.appendChild($TaskImg);
+                };
+                $newTaskDiv.appendChild($newTask);
+                $newSubject.appendChild($newTaskDiv);
+                $newSubject.appendChild(document.createElement('br'));
+            }
+        }
+        }
+    }
+);
+// function download(filename, text) {
+//     var pom = document.createElement('a');
+//     pom.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+//     pom.setAttribute('download', filename);
+//
+//     if (document.createEvent) {
+//         var event = document.createEvent('MouseEvents');
+//         event.initEvent('click', true, true);
+//         pom.dispatchEvent(event);
+//     }
+//     else {
+//         pom.click();
+//     }
+// }
+//
